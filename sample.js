dd95cc0fb86de8aff8e67f0d09c04c275ea3e3b8
Masaki Kono <masaki19931015@gmail.com>
2020-07-21 15:46:21 +0900

    add:Profile内容追加

--- sample.js
+++ sample.js
@@ -1,3 +1,15 @@
 $(function(){
     
 })
+
+$(function(){
+    $('a[href^="#"]').click(function(){
+      var speed = 500;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      // console.log($("html, body"));
+      $("html, body").animate({scrollTop:position}, speed, "swing");
+      return false;
+    });
+  });
